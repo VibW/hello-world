511d9e611a6ccfd2b9863fd3ca85702a293cfdef
AnjaliRathod <anjalirathod2@gmail.com>
2018-04-27 16:54:10 +0530

    commit by anjali

--- HelloFriday/src/com/prolifics/dummy/HelloWorld.java
+++ HelloFriday/src/com/prolifics/dummy/HelloWorld.java
@@ -3,7 +3,7 @@ package com.prolifics.dummy;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		System.out.println("hello world111111");
 
 	}
 
